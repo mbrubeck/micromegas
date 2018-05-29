ffc733d8acd98caa7b18d2c3f6a4e50f979b034d
Matt Brubeck <mbrubeck@limpet.net>
2018-05-29 14:22:36 -0700

    Add a FontFamily type

--- src/font.rs
+++ src/font.rs
@@ -1,28 +1,115 @@
+use euclid::Rect;
 use harfbuzz;
 
+// TODO: Use bit-packing to reduce size.
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
-struct FontStyle {
-    weight: u32,
-    variant: u32,
-    italic: bool,
+pub struct FontStyle {
+    pub weight: u32,
+    pub variant: u32,
+    pub italic: bool,
+    // TODO: language list ID
+}
+
+impl FontStyle {
+    /// Compute a matching metric between two styles. 0 is an exact match.
+    fn difference(&self, other: &Self) -> u32 {
+        self.weight - other.weight + if self.italic == other.italic { 0 } else { 2 }
+    }
+}
+
+impl Default for FontStyle {
+    fn default() -> Self {
+        FontStyle { weight: 4, variant: 0, italic: false }
+    }
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
-struct FontFakery {
-    fake_bold: bool,
-    fake_italic: bool,
+pub struct Fakery {
+    pub fake_bold: bool,
+    pub fake_italic: bool,
+}
+
+impl Fakery {
+    fn new<T>(wanted: FontStyle, actual: &Font<T>) -> Self {
+        Fakery {
+            fake_bold: wanted.weight >= 6 && (wanted.weight - actual.style.weight) >= 2,
+            fake_italic: wanted.italic && !actual.style.italic,
+        }
+    }
 }
 
 #[derive(Clone)]
-struct Options {
+pub struct Options {
     // font: &Font,
-    size: f32,
-    scale_x: f32,
-    skew_x: f32,
-    letter_spacing: f32,
-    word_spacing: f32,
+    pub size: f32,
+    pub scale_x: f32,
+    pub skew_x: f32,
+    pub letter_spacing: f32,
+    pub word_spacing: f32,
     // paintFlags
-    fakery: FontFakery,
+    pub fakery: Fakery,
     // hyphenEdit
-    features: harfbuzz::Features,
+    pub features: harfbuzz::Features,
+}
+
+pub trait Typeface {
+    fn h_advance(&self, glyph: u32, options: Options) -> f32;
+    fn bounds(&self, glyph: u32, options: Options) -> Rect<f32>;
+
+    // TODO: No longer needed when cached coverage is implemented.
+    fn has_glyph(&self, c: char) -> bool;
+}
+
+pub struct Font<T> {
+    pub typeface: T,
+    pub style: FontStyle,
+}
+
+pub struct FakedFont<'a, T: 'a> {
+    pub font: &'a Font<T>,
+    pub fakery: Fakery,
+}
+
+pub struct FontFamily<T> {
+    fonts: Vec<Font<T>>,
+    // lang_id
+    // variant
+    // supported_axes
+    // coverage (cached bit set)
+    // cmap_fmt14_coverage
+}
+
+impl<T> FontFamily<T> where T: Typeface {
+    pub fn new(fonts: Vec<Font<T>>) -> Self {
+        assert!(fonts.len() > 1, "FontFamily must contain at least one font");
+        FontFamily { fonts }
+    }
+
+    pub fn has_glyph(&self, c: char, /* _variation: u32 */) -> bool {
+        self.closest_match(FontStyle::default()).font.typeface.has_glyph(c)
+        // TODO: Use cached coverage tables instead of querying the typeface each time.
+        // TODO: Support variation selectors.
+    }
+
+    fn closest_match(&self, style: FontStyle) -> FakedFont<T> {
+        let font = self.fonts.iter().min_by_key(|f| f.style.difference(&style)).unwrap();
+        let fakery = Fakery::new(style, &font);
+        FakedFont { font, fakery }
+    }
+}
+
+pub struct FontCollection<T> {
+    pub families: Vec<FontFamily<T>>,
+}
+
+/// Break the text into runs of characters that can each be rendered with a single font from the
+/// given FontCollection.
+pub fn itemize<'a, T>(
+    _text: &str,
+    _style: FontStyle,
+    _fonts: &'a FontCollection<T>,
+) -> Vec<(FakedFont<'a, T>, &'a str)>
+    where T: Typeface
+{
+    Vec::new()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,15 @@
+extern crate euclid;
 extern crate harfbuzz;
 extern crate unicode_bidi as bidi;
+
+pub use font::{FontCollection, FontFamily, FontStyle, Typeface};
 
 mod font;
 mod word_break;
 
-pub fn layout_line(text: &str) {
+pub fn layout_line<T>(text: &str, style: FontStyle, fonts: &FontCollection<T>)
+    where T: Typeface
+{
     let bidi_info = bidi::BidiInfo::new(text, None);
 
     // Expect a single paragraph and treat it as one line.
@@ -12,19 +17,31 @@ pub fn layout_line(text: &str) {
     let paragraph = &bidi_info.paragraphs[0];
     let line = paragraph.range.clone();
 
+    // Iterate over bidi runs in visual order.
     let (bidi_levels, bidi_runs) = bidi_info.visual_runs(paragraph, line);
     for bidi_run in bidi_runs {
         let bidi_level = bidi_levels[bidi_run.start];
+
+        // Split each bidi run into "words" for caching purposes. If the same word occurs
+        // frequently, we can cache its layout rather than re-shaping it every time.
         for word in word_break::simple(&text[bidi_run]) {
-            layout_word(word, bidi_level);
+            layout_word(word, style, fonts, bidi_level);
         }
     }
 }
 
-pub fn layout_word(word: &str, bidi_level: bidi::Level) -> f32 {
+pub fn layout_word<T>(
+    word: &str,
+    style: FontStyle,
+    fonts: &FontCollection<T>,
+    _bidi_level: bidi::Level,
+) -> f32
+    where T: Typeface
+{
     let mut advance = 0.0;
 
-    // for font_run in font_collection.itemize(word, style)
+    for (_font, _run) in font::itemize(word, style, fonts) {
+    }
 
     advance
 }
